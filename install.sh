81ad370f821999e9217e96dacb308239cff7239f
Andy Smith <smith780@gmail.com>
2021-11-06 15:18:53 -0400

    Adds joplin keymap to config files

--- install.sh
+++ install.sh
@@ -57,3 +57,10 @@ ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
 [ ! -d "$XDG_CONFIG_HOME/tmux/plugins/tpm" ] \
     && git clone https://github.com/tmux-plugins/tpm \
     "$XDG_CONFIG_HOME/tmux/plugins/tpm"
+
+##########
+# Joplin #
+##########
+
+mkdir -p "$XDG_CONFIG_HOME/joplin"
+ln -sf "$DOTFILES/joplin/keymap.json" "$XDG_CONFIG_HOME/joplin"
